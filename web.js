280cd7e695b14ed5dadc287553af4dc4b607034c
Minji Ma <42285463+akalswl14@users.noreply.github.com>
2022-09-27 15:41:45 +0900

    Update web.js

--- web.js
+++ web.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 
 app.post('/', function (req, res) {
   console.log(req.body);
-  const moves = ['F', 'T', 'L', 'R'];
+  const moves = ['T','T','T','T'];
   
   // TODO add your implementation here to replace the random response
   
